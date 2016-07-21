e916f5ee1c87bfdcd8a78bce3afc2450cc88ca48
Jeremiah Androscavage <jcandroscavage@hotmail.com>
2016-07-20 23:21:10 -0400

    framing out codebase

---
+++ app/routes/home.php
@@ -0,0 +1,12 @@
+<?php
+
+$app->get('/', function () use ($app) {
+    $page = "Ops-API";
+    $meta = "Main Menu";
+
+    $app ->render('home.html.twig', [
+        'page' => $page,
+        'meta' => $meta
+    ]);
+
+})->name('home');;
