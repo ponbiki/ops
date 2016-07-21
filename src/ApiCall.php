c156fe1286225a5a376155b3f2085929d295b370
Jeremiah Androscavage <jcandroscavage@hotmail.com>
2016-07-21 01:12:29 -0400

    building API queries

---
+++ src/ApiCall.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ns1\ops;
+
+/**
+ * Api calls leveraging ops-api endpoints
+ * Class ApiCall
+ * @package ns1\ops
+ */
+class ApiCall implements iApiCall
+{
+    /**
+     * API request return body JSON
+     * @var string JSON return
+     */
+    protected $body;
+
+    /**
+     * @param $arg_array
+     * @return mixed
+     */
+    protected function baseCurl($arg_array)
+    {
+        $ch = \curl_init();
+        \curl_setopt($ch, \CURLOPT_URL, self::BASEURL . $arg_array['arg']);
+        if (isset($arg_array['opt'])) {
+            \curl_setopt($ch, \CURLOPT_POST, true);
+            \curl_setopt($ch, \CURLOPT_SAFE_UPLOAD, false);
+            \curl_setopt($ch, \CURLOPT_POSTFIELDS, $arg_array['opt']);
+            \curl_setopt($ch, \CURLOPT_HEADER, false);
+            \curl_setopt($ch, \CURLOPT_FOLLOWLOCATION, true);
+            \curl_setopt($ch, \CURLOPT_SSL_VERIFYPEER, false);
+        }
+        if (isset($arg_array['del'])) {
+            \curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, "DELETE");
+        }
+        \curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
+        $this->body = \json_decode(\curl_exec($ch));
+        \curl_close($ch);
+        return $this->body;
+    }
+}
---
+++ src/iApiCall.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ns1\ops;
+
+/**
+ * Interface iApiCall
+ * @package ns1\ops
+ */
+
+interface iApiCall
+{
+    /**
+     * Remote base URL for ops-api
+     * @const string Base URL of ops-api for baseCurl method
+     */
+    const BASEURL = 'https://ops-api.nsone.co/v1/';
+}
