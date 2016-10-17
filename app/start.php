f64e78e331f55456aaf70694415f0ce9ff048d2d
ponbiki <jcandroscavage@hotmail.com>
2016-10-17 17:20:53 -0400

    lots of changes   in non-working state

--- app/start.php
+++ app/start.php
@@ -1,18 +1,49 @@
 <?php
 
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+//
+//$app = new \Slim\Slim([
+//    'view' => new \Slim\Views\Twig()
+//]);
+//
+//// Views
+//$view = $app->view();
+//$view->setTemplatesDirectory('../app/views');
+//$view->parserExtensioons = [
+//    new \Slim\Views\TwigExtension()
+//];
+//
+//require 'routes.php';
+//
+//$app->run();
+
+$app = new \Slim\App();
+
+$container = $app->getContainer();
+
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('views/', [
+        'cache' => false
+    ]);
+
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
+
+/*
 require '../vendor/autoload.php';
 
-$app = new \Slim\Slim([
-    'view' => new \Slim\Views\Twig()
-]);
+$app = new \Slim\App;
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
 
-// Views
-$view = $app->view();
-$view->setTemplatesDirectory('../app/views');
-$view->parserExtensioons = [
-    new \Slim\Views\TwigExtension()
-];
-
-require 'routes.php';
-
+    return $response;
+});
 $app->run();
+*/
